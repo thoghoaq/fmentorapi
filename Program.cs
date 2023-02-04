c49af71a86d2ecdf57b5170be18335c4a11acfa5
ThongHT <thonght150201@gmail.com>
2023-02-04 21:21:52 +0700

    add swagger

--- Program.cs
+++ Program.cs
@@ -19,11 +19,9 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(builder => builder
                 .AllowAnyHeader()
