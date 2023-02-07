6e59c29b1ac6907a84060c818e27628f6865677e
ThongHT <thonght150201@gmail.com>
2023-02-08 00:33:33 +0700

    api sign in

--- DTOs/UserResponseModel.cs
+++ DTOs/UserResponseModel.cs
@@ -9,8 +9,14 @@ namespace FMentorAPI.DTOs
     {
         public UserResponseModel()
         {
-            Jobs = new HashSet<JobResponseModel>();
             Educations = new HashSet<EducationResponseModel>();
+            Jobs = new HashSet<JobResponseModel>();
+            Mentees = new HashSet<MenteeResponseModel>();
+            Mentors = new HashSet<MentorResponseModel>();
+            Payments = new HashSet<PaymentResponseModel>();
+            ReviewReviewees = new HashSet<ReviewResponseModel>();
+            ReviewReviewers = new HashSet<ReviewResponseModel>();
+            UserSpecialties = new HashSet<UserSpecialtyResponseModel>();
         }
         public int UserId { get; set; }
         public string Name { get; set; } = null!;
@@ -22,7 +28,15 @@ namespace FMentorAPI.DTOs
         public byte IsMentor { get; set; }
         public string? Photo { get; set; }
 
-        public virtual ICollection<JobResponseModel> Jobs { get; set; }
+        public virtual UserPermissionResponseModel IsMentorNavigation { get; set; } = null!;
+        public virtual WalletResponseModel? Wallet { get; set; }
         public virtual ICollection<EducationResponseModel> Educations { get; set; }
+        public virtual ICollection<JobResponseModel> Jobs { get; set; }
+        public virtual ICollection<MenteeResponseModel> Mentees { get; set; }
+        public virtual ICollection<MentorResponseModel> Mentors { get; set; }
+        public virtual ICollection<PaymentResponseModel> Payments { get; set; }
+        public virtual ICollection<ReviewResponseModel> ReviewReviewees { get; set; }
+        public virtual ICollection<ReviewResponseModel> ReviewReviewers { get; set; }
+        public virtual ICollection<UserSpecialtyResponseModel> UserSpecialties { get; set; }
     }
 }
