f00e633106c000cbaa32c6948aee8038f3a992e8
ThongHT <thonght150201@gmail.com>
2023-02-19 01:50:10 +0700

    update cron

--- FMetorAPI.WebbAPI/Extensions/Cron/UpdateAppointmentStatus.cs
+++ FMetorAPI.WebbAPI/Extensions/Cron/UpdateAppointmentStatus.cs
@@ -16,9 +16,17 @@ namespace FMentorAPI.WebAPI.Extensions.Cron
         }
         public Task Execute(IJobExecutionContext context)
         {
+            StartMeeting();
+            EndMeeting();
+            //Write your custom code here
+            return Task.FromResult(true);
+        }
+
+        private void EndMeeting()
+        {
             try
             {
-                var ovedue = _dbcontext.Appointments.Where(a => a.StartTime.AddMinutes(a.Duration) >= DateTime.Now && a.Status.Equals("Accepted")).ToList();
+                var ovedue = _dbcontext.Appointments.Where(a => a.StartTime.AddMinutes(a.Duration) >= DateTime.Now && a.Status.Equals("Happenning")).ToList();
                 foreach (var item in ovedue)
                 {
                     item.Status = "Completed";
@@ -56,16 +64,68 @@ namespace FMentorAPI.WebAPI.Extensions.Cron
                                 };
                                 _notificationService.SendNotification(notificationModel);
                             }
-                            
+
                         }
                     }
                 }
-            }catch(Exception ex)
+            }
+            catch (Exception ex)
             {
                 throw;
             }
-            //Write your custom code here
-            return Task.FromResult(true);
+        }
+
+        private void StartMeeting()
+        {
+            try
+            {
+                var ovedue = _dbcontext.Appointments.Where(a => a.StartTime <= DateTime.Now && a.Status.Equals("Accepted")).ToList();
+                foreach (var item in ovedue)
+                {
+                    item.Status = "Happenning";
+                    var entity = _dbcontext.Appointments.Update(item);
+                    _dbcontext.SaveChanges();
+                    NotificationRequestModel notificationModel;
+                    var mentee = _dbcontext.Mentees.FirstOrDefault(m => m.MenteeId == entity.Entity.MenteeId);
+                    var mentor = _dbcontext.Mentors.FirstOrDefault(m => m.MentorId == entity.Entity.MentorId);
+                    if (mentee != null && mentor != null)
+                    {
+                        {
+                            var token1 = _dbcontext.UserTokens.FirstOrDefault(u => u.UserId == mentor.UserId);
+                            var token = _dbcontext.UserTokens.FirstOrDefault(u => u.UserId == mentee.UserId);
+                            if (token1 != null)
+                            {
+                                notificationModel = new NotificationRequestModel
+                                {
+                                    DeviceId = token1.Token,
+                                    IsAndroiodDevice = true,
+                                    Title = "The appointment is happenning!",
+                                    Body = "The appointment is happenning!",
+                                    Route = token1.Token
+                                };
+                                _notificationService.SendNotification(notificationModel);
+                            }
+                            if (token != null)
+                            {
+                                notificationModel = new NotificationRequestModel
+                                {
+                                    DeviceId = token.Token,
+                                    IsAndroiodDevice = true,
+                                    Title = "The appointment is happenning!",
+                                    Body = "The appointment is happenning!",
+                                    Route = "mentee"
+                                };
+                                _notificationService.SendNotification(notificationModel);
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
