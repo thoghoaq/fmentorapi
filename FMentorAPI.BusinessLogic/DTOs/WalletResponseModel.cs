e587071aa82c2abece90a58936d082084f2aa060
QUANGNT\quang <quangnt1702@gmail.com>
2023-02-18 17:12:28 +0700

    Add WalletId response when login

--- FMentorAPI.BusinessLogic/DTOs/WalletResponseModel.cs
+++ FMentorAPI.BusinessLogic/DTOs/WalletResponseModel.cs
@@ -1,7 +1,10 @@
-﻿namespace FMentorAPI.BusinessLogic.DTOs
+﻿using FMentorAPI.DataAccess.Models;
+
+namespace FMentorAPI.BusinessLogic.DTOs
 {
     public class WalletResponseModel
     {
+        public int WalletId { get; set; }
         public int UserId { get; set; }
         public decimal Balance { get; set; }
     }
