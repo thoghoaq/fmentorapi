b08eaad57a99616ee22c785e3a715a1af6b94ecd
ThongHT <thonght150201@gmail.com>
2023-02-19 08:21:32 +0700

    revert cron

--- FMetorAPI.WebbAPI/Extensions/Cron/UpdateAppointmentStatus.cs
+++ FMetorAPI.WebbAPI/Extensions/Cron/UpdateAppointmentStatus.cs
@@ -16,17 +16,9 @@ namespace FMentorAPI.WebAPI.Extensions.Cron
         }
         public Task Execute(IJobExecutionContext context)
         {
-            StartMeeting();
-            EndMeeting();
-            //Write your custom code here
-            return Task.FromResult(true);
-        }
-
-        private void EndMeeting()
-        {
             try
             {
-                var ovedue = _dbcontext.Appointments.Where(a => a.StartTime.AddMinutes(a.Duration) <= DateTime.Now && DateTime.Now < a.StartTime.AddMinutes(a.Duration + 5) && a.Status.Equals("Happenning")).ToList();
+                var ovedue = _dbcontext.Appointments.Where(a => a.StartTime.AddMinutes(a.Duration) >= DateTime.Now && a.Status.Equals("Accepted")).ToList();
                 foreach (var item in ovedue)
                 {
                     item.Status = "Completed";
@@ -64,68 +56,16 @@ namespace FMentorAPI.WebAPI.Extensions.Cron
                                 };
                                 _notificationService.SendNotification(notificationModel);
                             }
-
+                            
                         }
                     }
                 }
-            }
-            catch (Exception ex)
+            }catch(Exception ex)
             {
                 throw;
             }
-        }
-
-        private void StartMeeting()
-        {
-            try
-            {
-                var ovedue = _dbcontext.Appointments.Where(a => a.StartTime.AddMinutes(0) <= DateTime.Now && DateTime.Now < a.StartTime.AddMinutes(5) && a.Status.Equals("Accepted")).ToList();
-                foreach (var item in ovedue)
-                {
-                    item.Status = "Happenning";
-                    var entity = _dbcontext.Appointments.Update(item);
-                    _dbcontext.SaveChanges();
-                    NotificationRequestModel notificationModel;
-                    var mentee = _dbcontext.Mentees.FirstOrDefault(m => m.MenteeId == entity.Entity.MenteeId);
-                    var mentor = _dbcontext.Mentors.FirstOrDefault(m => m.MentorId == entity.Entity.MentorId);
-                    if (mentee != null && mentor != null)
-                    {
-                        {
-                            var token1 = _dbcontext.UserTokens.FirstOrDefault(u => u.UserId == mentor.UserId);
-                            var token = _dbcontext.UserTokens.FirstOrDefault(u => u.UserId == mentee.UserId);
-                            if (token1 != null)
-                            {
-                                notificationModel = new NotificationRequestModel
-                                {
-                                    DeviceId = token1.Token,
-                                    IsAndroiodDevice = true,
-                                    Title = "The appointment is happenning!",
-                                    Body = "The appointment is happenning!",
-                                    Route = token1.Token
-                                };
-                                _notificationService.SendNotification(notificationModel);
-                            }
-                            if (token != null)
-                            {
-                                notificationModel = new NotificationRequestModel
-                                {
-                                    DeviceId = token.Token,
-                                    IsAndroiodDevice = true,
-                                    Title = "The appointment is happenning!",
-                                    Body = "The appointment is happenning!",
-                                    Route = "mentee"
-                                };
-                                _notificationService.SendNotification(notificationModel);
-                            }
-
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
+            //Write your custom code here
+            return Task.FromResult(true);
         }
     }
 }
