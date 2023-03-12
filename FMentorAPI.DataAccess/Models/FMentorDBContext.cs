f9879289d5fc4540767c5d73256ca92d5ddf2bfe
ThongHT <thonght150201@gmail.com>
2023-03-12 17:47:25 +0700

    change db

--- FMentorAPI.DataAccess/Models/FMentorDBContext.cs
+++ FMentorAPI.DataAccess/Models/FMentorDBContext.cs
@@ -42,7 +42,7 @@ namespace FMentorAPI.DataAccess.Models
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https: //go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer(
-                    "Server=tcp:fmentor-sea.database.windows.net,1433;Initial Catalog=FMentorDatabase;Persist Security Info=False;User ID=sqladmin;Password=adpass01@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                    "Server=tcp:fmentor.database.windows.net,1433;Initial Catalog=FMentorDatabase;Persist Security Info=False;User ID=fmentor;Password=adpass01@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
